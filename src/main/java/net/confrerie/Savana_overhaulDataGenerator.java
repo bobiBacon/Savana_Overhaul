ccbd18313feb8e9123861193f27bf33f5c71d74a
bob_bacon <antoinedsa.6@gmail.com>
2024-06-27 18:31:27 -0400

    ajout du mod

---
+++ src/main/java/net/confrerie/Savana_overhaulDataGenerator.java
@@ -0,0 +1,11 @@
+package net.confrerie;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Savana_overhaulDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
